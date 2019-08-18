5423349d5c582efad74a21e772fb9ed18ad2d3c3
Igor Ovinov <market1942@mail.ru>
2019-08-18 18:45:07 +0300

    Updates

---
+++ precache-manifest.5d5223f441eed991bde09d60ef55ddb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946e521f27b6850188841a13e06f0f43",
+    "url": "/diabet/index.html"
+  },
+  {
+    "revision": "f6c208e1e1114a08a092",
+    "url": "/diabet/static/css/main.cd45d06e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/diabet/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f6c208e1e1114a08a092",
+    "url": "/diabet/static/js/main.ca971467.chunk.js"
+  },
+  {
+    "revision": "83cd52aa4e472535d39a",
+    "url": "/diabet/static/js/runtime~main.d4ffa131.js"
+  },
+  {
+    "revision": "cebc704abd2d0b0c5f976f9489d631cb",
+    "url": "/diabet/static/media/background.cebc704a.png"
+  },
+  {
+    "revision": "be75dbe6404d47e073407791323d012f",
+    "url": "/diabet/static/media/ice1.be75dbe6.png"
+  },
+  {
+    "revision": "8212b9def30aafb36d49654c46c3544b",
+    "url": "/diabet/static/media/ref.8212b9de.png"
+  }
+]);
